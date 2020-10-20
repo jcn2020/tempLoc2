817234786ace77bcf9e61af024933a45d8651ab9
jcn2020 <jcn2020@gmail.com>
2020-10-19 18:09:24 -0700

    Update genericbuild.groovy

--- vars/genericbuild.groovy
+++ vars/genericbuild.groovy
@@ -1,11 +1,11 @@
 def call(Map config=[:]) {
     node {
         stage("Say You") {
-            println("Say You") 
+            println("Say You " + config.target ) 
         } // stage
         
         stage("Say Me") {
-            println("Say Me")
+            println("Say Me " + config.target )
         } // stage
     } // node
 } // call 
